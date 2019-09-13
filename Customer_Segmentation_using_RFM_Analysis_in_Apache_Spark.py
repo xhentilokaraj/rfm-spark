86fe3ee2cb11c7db70f28df8c26319a3519685a3
unknown <x.karaj@studiosynthesis.biz>
2019-09-13 10:29:59 +0200

    final version

--- Customer_Segmentation_using_RFM_Analysis_in_Apache_Spark.py
+++ Customer_Segmentation_using_RFM_Analysis_in_Apache_Spark.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[461]:
+# In[72]:
 
 
 import findspark
@@ -12,6 +12,8 @@ import pandas as pd
 get_ipython().run_line_magic('load_ext', 'autotime')
 
 import chart_studio.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 
 import pyspark
@@ -21,13 +23,13 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
 
-# In[6]:
+# In[4]:
 
 
 spark = SparkSession     .builder     .appName("RFM Analysis with PySpark")     .getOrCreate()
 
 
-# In[7]:
+# In[5]:
 
 
 spark
@@ -35,73 +37,67 @@ spark
 
 # Data Source: https://www.kaggle.com/carrie1/ecommerce-data
 
-# In[232]:
+# In[6]:
 
 
 data = spark.read.format("csv").option("header", "true").load("data.csv")
 
 
-# In[233]:
+# In[7]:
 
 
 data
 
 
-# In[234]:
+# In[8]:
 
 
 data.columns
 
 
-# In[235]:
+# In[9]:
 
 
 data.printSchema()
 
 
-# In[237]:
+# In[10]:
 
 
 # cache data in memory
 data.cache().count()
 
 
-# In[432]:
+# In[33]:
 
 
-data.groupBy("RecencyDays").count().show()
-
-
-# In[426]:
-
-
-data.show()
+data.show(5)
 
 
 # # 1 Data Pre-Processing
 
-# In[239]:
+# In[13]:
 
 
 data = data.withColumn("Quantity", data["Quantity"].cast(IntegerType()))
 data = data.withColumn("UnitPrice", data["UnitPrice"].cast(DoubleType()))
 
 
-# In[240]:
+# In[14]:
 
 
 # define Total column
 data = data.withColumn("Total", round(data["UnitPrice"] * data["Quantity"], 2))
 
 
-# In[241]:
+# In[15]:
 
 
 # change date format
 data = data.withColumn("Date", to_date(unix_timestamp("InvoiceDate", "MM/dd/yyyy").cast("timestamp")))
 
 
-# In[416]:
+# In[16]:
 
 
 # calculate difference in days between 2011-12-31 and the Invoice Date
@@ -110,27 +106,33 @@ data = data.withColumn("RecencyDays", expr("datediff('2011-12-31', Date)"))
 
 # # 2 Create RFM Table
 
-# In[436]:
+# In[50]:
 
 
 # Creation of RFM table
 
-rfm_table = data.groupBy("CustomerId")                        .agg(max("Date").alias("LastPurchase"),                              min("RecencyDays").alias("Recency"),                              count("InvoiceNo").alias("Frequency"),                              sum("Total").alias("Monetary"))
+rfm_table = data.groupBy("CustomerId")                        .agg(min("RecencyDays").alias("Recency"),                              count("InvoiceNo").alias("Frequency"),                              sum("Total").alias("Monetary"))
 
 
-# In[437]:
+# In[51]:
+
+
+rfm_table = rfm_table.withColumn("Monetary", round(rfm_table["Monetary"], 2))
+
+
+# In[52]:
 
 
 rfm_table.printSchema()
 
 
-# In[438]:
+# In[54]:
 
 
 rfm_table.show(5)
 
 
-# In[441]:
+# In[20]:
 
 
 rfm_table.cache().count()
@@ -138,7 +140,7 @@ rfm_table.cache().count()
 
 # # 3 Computing Quartiles of RFM values
 
-# In[442]:
+# In[56]:
 
 
 r_quartile = rfm_table.approxQuantile("Recency", [0.25, 0.5, 0.75], 0)
@@ -146,7 +148,7 @@ f_quartile = rfm_table.approxQuantile("Frequency", [0.25, 0.5, 0.75], 0)
 m_quartile = rfm_table.approxQuantile("Monetary", [0.25, 0.5, 0.75], 0)
 
 
-# In[446]:
+# In[57]:
 
 
 # calculate Recency based on quartile
@@ -154,7 +156,7 @@ m_quartile = rfm_table.approxQuantile("Monetary", [0.25, 0.5, 0.75], 0)
 rfm_table = rfm_table.withColumn("R_Quartile",                                  when(col("Recency") >= r_quartile[2] , 1).                                 when(col("Recency") >= r_quartile[1] , 2).                                 when(col("Recency") >= r_quartile[0] , 3).                                 otherwise(4))
 
 
-# In[447]:
+# In[58]:
 
 
 # calculate Frequency based on quartile
@@ -162,7 +164,7 @@ rfm_table = rfm_table.withColumn("R_Quartile",                                  
 rfm_table = rfm_table.withColumn("F_Quartile",                                  when(col("Frequency") > f_quartile[2] , 4).                                 when(col("Frequency") > f_quartile[1] , 3).                                 when(col("Frequency") > f_quartile[0] , 2).                                 otherwise(1))
 
 
-# In[448]:
+# In[59]:
 
 
 # calculate Monetary based on quartile
@@ -170,7 +172,7 @@ rfm_table = rfm_table.withColumn("F_Quartile",                                  
 rfm_table = rfm_table.withColumn("M_Quartile",                                  when(col("Monetary") >= m_quartile[2] , 4).                                 when(col("Monetary") >= m_quartile[1] , 3).                                 when(col("Monetary") >= m_quartile[0] , 2).                                 otherwise(1))
 
 
-# In[449]:
+# In[60]:
 
 
 # combine the scores (R_Quartile, F_Quartile,M_Quartile) together.
@@ -178,31 +180,31 @@ rfm_table = rfm_table.withColumn("M_Quartile",                                  
 rfm_table = rfm_table.withColumn("RFM_Score", concat(col("R_Quartile"), col("F_Quartile"), col("M_Quartile")))
 
 
-# In[450]:
+# In[62]:
 
 
-rfm_table.show(5)
+rfm_table.show(10)
 
 
 # # 4 RFM Analysis
 
-# In[457]:
+# In[63]:
 
 
 # Best customers
 
-rfm_table.select("CustomerID").where("RFM_Score == 444").show(11)
+rfm_table.select("CustomerID").where("RFM_Score == 444").show(10)
 
 
-# In[467]:
+# In[64]:
 
 
 # group by RFM Score
 
-grouped_by_rfmscore = rfm_table.groupBy("R_Quartile", "F_Quartile", "M_Quartile").count().orderBy("count", ascending=False)
+grouped_by_rfmscore = rfm_table.groupBy("RFM_Score").count().orderBy("count", ascending=False)
 
 
-# In[468]:
+# In[65]:
 
 
 # convert Spark dataframe to pandas in order to visualize data
@@ -210,19 +212,19 @@ grouped_by_rfmscore = rfm_table.groupBy("R_Quartile", "F_Quartile", "M_Quartile"
 grouped_by_rfmscore_pandas = grouped_by_rfmscore.toPandas()
 
 
-# In[469]:
+# In[66]:
 
 
 grouped_by_rfmscore_pandas
 
 
-# In[453]:
+# In[67]:
 
 
 grouped_by_rfmscore_pandas['RFM_Score'] = "Seg " + grouped_by_rfmscore_pandas['RFM_Score'].map(str)
 
 
-# In[462]:
+# In[73]:
 
 
 data = [go.Bar(x=grouped_by_rfmscore_pandas['RFM_Score'], y=grouped_by_rfmscore_pandas['count'])]
@@ -245,4 +247,10 @@ layout = go.Layout(
 
 fig = go.Figure(data=data, layout=layout)
 iplot(fig, filename='rfm_Segments')
+
+
+# In[ ]:
+
+
+
 
